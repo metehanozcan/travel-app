7c5b1c8cddda2e5ac341f93cbdb0622128bf8241
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2022-09-28 03:35:25 +0300

    Bottom Navigation View Implemented

---
+++ app/src/main/java/com/kenbu/travelapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.kenbu.travelapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.kenbu.travelapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.apply {
+            bottomNavigationView.setOnApplyWindowInsetsListener(null)
+            bottomNavigationView.setPadding(12,12,12,12)
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(fragmentContainerView.id) as NavHostFragment
+            NavigationUI.setupWithNavController(
+                binding.bottomNavigationView,
+                navHostFragment.navController
+            )
+        }
+
+    }
+}
